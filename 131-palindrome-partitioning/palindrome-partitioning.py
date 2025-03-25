eda3dde8614913099d322b0a746a2d0e878e45a8
Sk Shakeel Akhtar <skkafilakhtar786@gmail.com>
2025-03-25 11:30:31 +0530

    Time: 91 ms (29.43%) | Memory: 47.9 MB (62.48%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        def is_palindrome(sub):
+            return sub == sub[::-1]
+
+        def backtrack(start, path):
+            if start == len(s):
+                result.append(path[:])
+                return
+            for end in range(start + 1, len(s) + 1):
+                if is_palindrome(s[start:end]):
+                    backtrack(end, path + [s[start:end]])
+
+        result = []
+        backtrack(0, [])
+        return result
+        
+sol = Solution()
+print(sol.partition("aab"))  
+print(sol.partition("a"))    
