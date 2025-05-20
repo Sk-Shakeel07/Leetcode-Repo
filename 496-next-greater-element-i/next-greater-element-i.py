9702796ccc85c685454533a129f7f8893540bd76
Sk Shakeel Akhtar <skkafilakhtar786@gmail.com>
2025-05-20 12:30:05 +0530

    Time: 42 ms (22.79%) | Memory: 12.6 MB (85.86%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        l = []
+        for i in nums1:
+            if i == nums2[-1]:
+                l.append(-1)
+            else:
+                for j in range(nums2.index(i) + 1, len(nums2)):
+                    if nums2[j] > i:
+                        l.append(nums2[j])
+                        break
+                else:
+                    l.append(-1)
+        return l
