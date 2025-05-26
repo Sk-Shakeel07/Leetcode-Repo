a8a8fbaf78397947a64c569c009051809955e184
Sk Shakeel Akhtar <skkafilakhtar786@gmail.com>
2025-05-26 20:25:59 +0530

    Time: 23 ms (70.87%) | Memory: 15 MB (50.49%) - LeetSync

---
+++ 539-minimum-time-difference/minimum-time-difference.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findMinDifference(self, timePoints):
+        """
+        :type timePoints: List[str]
+        :rtype: int
+        """
+        minutes = []
+        for time in timePoints:
+            h, m = map(int, time.split(':'))
+            minutes.append(h * 60 + m)
+        
+        # Sort times in ascending order
+        minutes.sort()
+        
+        # Find minimum difference across adjacent elements
+        min_diff = float('inf')
+        for i in range(len(minutes) - 1):
+            min_diff = min(min_diff, minutes[i + 1] - minutes[i])
+        
+        # Consider the circular difference between last and first element
+        min_diff = min(min_diff, 24 * 60 - minutes[-1] + minutes[0])
+        
+        return min_diff
