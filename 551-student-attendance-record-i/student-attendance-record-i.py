f5c9238a35895d2b1000762a08d8dcfa0b1accc3
Sk Shakeel Akhtar <skkafilakhtar786@gmail.com>
2025-06-17 19:26:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (10.50%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if s.count('A') >= 2:
+            return False
+        if "LLL" in s:
+            return False
+        return True
