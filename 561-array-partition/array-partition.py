ed3dd63b58dba423a729bb23286989b41d34999b
Sk Shakeel Akhtar <skkafilakhtar786@gmail.com>
2025-06-18 11:47:02 +0530

    Time: 70 ms (8.01%) | Memory: 14.1 MB (29.80%) - LeetSync

---
+++ 561-array-partition/array-partition.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        bucket = [0] * 20001
+        for x in nums:
+            bucket[x + 10000] += 1
+        res = 0
+        flag = True
+        for i in range(20001):
+            while bucket[i]:
+                if flag:
+                    res += i - 10000
+                flag = not flag
+                bucket[i] -= 1
+        return res
