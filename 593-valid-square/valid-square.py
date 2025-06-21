117e84ffce718cab69552e02a34dcda864f99bdf
Sk Shakeel Akhtar <skkafilakhtar786@gmail.com>
2025-06-21 20:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (86.36%) - LeetSync

---
+++ 593-valid-square/valid-square.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def validSquare(self, p1, p2, p3, p4):
+        """
+        :type p1: List[int]
+        :type p2: List[int]
+        :type p3: List[int]
+        :type p4: List[int]
+        :rtype: bool
+        """
+        if p1==p2==p3==p4:return False
+        def dist(x,y):
+            return (x[0]-y[0])**2+(x[1]-y[1])**2
+        ls=[dist(p1,p2),dist(p1,p3),dist(p1,p4),dist(p2,p3),dist(p2,p4),dist(p3,p4)]
+        ls.sort()
+        if ls[0]==ls[1]==ls[2]==ls[3]:
+            if ls[4]==ls[5]:
+                return True
+        return False
