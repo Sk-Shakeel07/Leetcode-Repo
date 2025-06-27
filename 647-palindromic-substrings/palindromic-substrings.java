f296364da8540cca38bd054f43a737b63c2ee63c
Sk Shakeel Akhtar <skkafilakhtar786@gmail.com>
2025-06-27 19:02:56 +0530

    Time: 2 ms (99.35%) | Memory: 41.5 MB (73.37%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countSubstrings(String s) {
+        if(s.length() == 0) return 0;
+        int n = s.length();
+        int res = 0 ;
+        char[] c= s.toCharArray();
+        
+        for(int i = 0 ; i < n ; i++){
+            //Odd and even lengths (Center for odd --> i , i)
+            //Center for even --> i , i+1
+            res += isPalindrome(i , i , c);
+            res += isPalindrome(i, i + 1 , c);
+        }
+        return res;
+    }
+    
+    public int isPalindrome(int s , int e , char[] c){
+        int count = 0;
+        while(s >= 0 && e < c.length && c[s--] == c[e++])
+            count++;
+        
+        return count;
+    }
+}
