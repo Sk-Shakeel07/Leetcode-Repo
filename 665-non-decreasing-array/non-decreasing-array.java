f1601a6902c686134f90ef2db9e58e479f1ce03f
Sk Shakeel Akhtar <skkafilakhtar786@gmail.com>
2025-06-30 18:58:31 +0530

    Time: 1 ms (32.64%) | Memory: 45.5 MB (42.72%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {        
+        // corner case
+        if(nums == null || nums.length <= 1) return true;
+        
+        // the count of continuous non-decreasing sub-array
+        int count = 1;
+        int index = 0;
+        
+        int n = nums.length;        
+        for(int i = 1; i < n; i++){
+            if(nums[i] < nums[i - 1]){
+                index = i;
+                count++;
+            }
+        }
+        
+        if(count == 1) return true;
+        if(count == 2){ 
+            // if the discord happens at start or end position, we can modify
+            // it to match the condition
+            if(index == 1 || index == n - 1) return true;
+            
+            // if in the middle, we modify nums[index - 2]
+            // index - 2, (index - 1, index) , index + 1
+            if(nums[index + 1] >= nums[index - 1] || nums[index] >= nums[index - 2]) {
+                return true;
+            }
+            return false;
+        }
+        
+        return false;
+    }
+}
