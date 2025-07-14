8485799fb1e37537c7112b1dacf07b94d6a004bf
Sk Shakeel Akhtar <skkafilakhtar786@gmail.com>
2025-07-14 19:09:14 +0530

    Time: 78 ms (56.04%) | Memory: 12.6 MB (89.71%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        def is_palindrome(s, left, right):
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                left += 1
+                right -= 1
+            return True
+
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return is_palindrome(s, left + 1, right) or is_palindrome(s, left, right - 1)
+            left += 1
+            right -= 1
+
+        return True
